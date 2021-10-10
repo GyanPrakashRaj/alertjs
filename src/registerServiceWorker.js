0ea50246fd286d66e97675d5f141142b03254b83
Gyan Prakash <79048275+GyanPrakashRaj@users.noreply.github.com>
2021-10-10 22:05:49 +0530

    Sevice js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,4 +1,4 @@
-// In production, we register a service worker to serve assets from local cache.
+// LOCAL CACHE __--___-GPR-
 
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
