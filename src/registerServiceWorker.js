d050495e0881153f50d93b26ccb3ef283dee3be0
Gyan Prakash <79048275+GyanPrakashRaj@users.noreply.github.com>
2021-10-10 22:10:43 +0530

    Update registerServiceWorker.js

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,7 +2,7 @@
 
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on the "N+1" visit to a page, since previously
+// will only see deployed updates on the "N+1" visit to a page, since previously the master algorithm.
 // cached resources are updated in the background.
 
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
